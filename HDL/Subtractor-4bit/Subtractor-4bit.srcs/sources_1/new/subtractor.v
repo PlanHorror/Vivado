d918993129bd67157fb71cf387c1f5295c88919b
planhorror <nmtxma2004@gmail.com>
2025-03-03 15:07:06 +0700

    Bram read_first

--- HDL/Subtractor-4bit/Subtractor-4bit.srcs/sources_1/new/subtractor.v
+++ HDL/Subtractor-4bit/Subtractor-4bit.srcs/sources_1/new/subtractor.v
@@ -27,12 +27,15 @@ module subtractor(
     );
     wire [3:0] b_in;
     wire [3:0] c_out1;
-    assign b_in = ~b;
+    not(b_in[0], b[0]);
+    not(b_in[1], b[1]);
+    not(b_in[2], b[2]);
+    not(b_in[3], b[3]);
     full_adder fa0(.a(a[0]), .b(b_in[0]), .cin(1), .sum(diff[0]), .cout(c_out1[0]));
     full_adder fa1(.a(a[1]), .b(b_in[1]), .cin(c_out1[0]), .sum(diff[1]), .cout(c_out1[1]));
     full_adder fa2(.a(a[2]), .b(b_in[2]), .cin(c_out1[1]), .sum(diff[2]), .cout(c_out1[2]));
     full_adder fa3(.a(a[3]), .b(b_in[3]), .cin(c_out1[2]), .sum(diff[3]), .cout(c_out1[3]));
-    assign c_out = c_out1[3];
+    not( c_out , c_out1[3]);
     
 
 
